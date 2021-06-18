14f9bcecfcfd6aa163e2e8495d6684c8acdfe5c8
shresth711 <gupta.shresth864p@gmail.com>
2021-06-18 17:57:19 +0530

    add

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var canvas, backgroundImage;
+var cars , car1, car2 , car3, car4;
 
 var gameState = 0;
 var playerCount;
@@ -10,7 +11,7 @@ var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
